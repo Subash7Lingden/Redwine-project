f2107e711b1e6d74e0683fbd15a340dce812d514
Subash7Lingden <subashsubbalingden@gmail.com>
2024-04-18 22:46:10 +0545

    data transformation added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestionn import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 
@@ -26,5 +27,15 @@ if __name__ =="__main__":
         logger.exception(e)
         raise e
 
+STAGE_NAME = "Data Transformation Stage"
+if __name__ =="__main__":
+    try:
+        logger.info("f>>>>>>>> stage {STAGE_NAME} started <<<<<<<")
+        data_transformation = DataTransformationTrainingPipeline()
+        data_transformation.main()
+        logger.info(f">>>>>>>>>stage {STAGE_NAME} completed <<<<<\n\nx========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
 
 
