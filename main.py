0ec4e664256eb549a10cb2001c053dbeee4cdddf
Subash7Lingden <subashsubbalingden@gmail.com>
2024-04-19 08:41:53 +0545

    model evaluation added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from mlProject.pipeline.stage_01_data_ingestionn import DataIngestionTrainingPip
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
 
@@ -52,3 +53,13 @@ if __name__ =="__main__":
         raise e
 
 
+STAGE_NAME ="Model Evaluation Stage"
+if __name__=="__main__":
+    try:
+        logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<")
+        model_evaluation= ModelEvaluationTrainingPipeline()
+        model_evaluation.main()
+        logger.info(f" >>>>>>>>> {STAGE_NAME} completed <<<<<<<<<<\n\nx=======x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
